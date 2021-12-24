377155f12427dc2752c72e1545b83e91df171e88
Nguyen Thinh <ng.tr.thinh@gmail.com>
2021-12-24 20:57:24 +0700

    query child accounts by token

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -15,6 +16,6 @@ class AccountAPI(APIView):
 class ChildAccountAPI(APIView):
     def get(self, request):
         children_account = Account.objects.filter(
-            managed_by=request.data.get('account_id'))
+            Q(managed_by__user=request.user))
         serializer = AccountSerializer(children_account, many=True)
         return Response(data=serializer.data)
